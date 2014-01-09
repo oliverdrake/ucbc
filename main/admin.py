ad921958c710b6558092df98879d931ec7b459e9
oliverdrake <oliver@drake.ch>
2014-01-09 23:23:35 +1300

    Fixes #4 Extending UserAdmin to display more stuff in the list view. Thinking we can just use some permission groups to organise users, don't really need any custom permissions.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+class UCBCUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'date_joined',
+                    'is_staff', 'is_active', 'is_superuser', 'get_groups')
+
+    def get_groups(self, user):
+        return ", ".join([str(group.name) for group in user.groups.all()])
+    get_groups.short_description = "Groups"
+
+admin.site.unregister(get_user_model())
+admin.site.register(get_user_model(), UCBCUserAdmin)
