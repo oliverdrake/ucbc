cadc45ba6f216151895690f0ad586a553931eccd
Oliver Drake <oliver.drake@adscale.co.nz>
2014-01-23 09:28:03 +1300

    removing aws stuff

--- scripts/backup_sqlite.sh
+++ scripts/backup_sqlite.sh
@@ -2,4 +2,4 @@
 cd ~/sites/ucbc/ucbc
 backup_file="/tmp/sqlite3_dump_$(date --utc +%FT%T).bak"
 sqlite3 db.sqlite3 .dump > $backup_file
-boto-rsync -a AKIAJ4F55224DJHO4IOA -s n1q/IUsqFORjA/GLuZmGLJibegKcKyC5zJi5eZ3f $backup_file s3://ucbcbackups/
+boto-rsync -a key -s secret $backup_file s3://ucbcbackups/
