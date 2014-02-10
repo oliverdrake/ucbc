437a010d4f2a29497b0d43a174f55028ad19e96e
oliverdrake <oliver@drake.ch>
2014-02-10 21:49:59 +1300

    enabling allauth emails for now

--- main/other.py
+++ main/other.py
@@ -4,4 +4,5 @@ from allauth.account.adapter import DefaultAccountAdapter
 class AuthAdaptor(DefaultAccountAdapter):
     def send_mail(self, template_prefix, email, context):
         # Disabling emails for now as my custom script sends the email to new members
-        pass
+        # pass
+        return super(AuthAdaptor, self).send_mail(template_prefix, email, context)
